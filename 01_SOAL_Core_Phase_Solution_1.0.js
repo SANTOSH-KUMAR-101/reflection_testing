ca933a7c16929072717ea9c4535745f18b763bcd
SANTOSH KUMAR <santosk.kumar.101@gmail.com>
2021-12-02 21:35:08 +0530

    Update and rename 01_SOAL_Core_Phase_Solution.js to 01_SOAL_Core_Phase_Solution_1.0.js

---
+++ 01_SOAL_Core_Phase_Solution_1.0.js
@@ -0,0 +1,11 @@
+// Question 1:-
+// Create a function tjat accepts an array called 'myarray' as an argument, iterates through 'myarray', 
+// and returns a new array which contains all the even number in 'myarray'.
+// Sample input: [1,2,3,4,5,6,7,8]
+// Sample output: [2,4,6,8]
+
+
+// Sol:-
+  
+    
+  
