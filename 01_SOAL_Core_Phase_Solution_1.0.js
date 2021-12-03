2229d3fe22262b76bb81d8c60c2dac1b55590d25
SANTOSH KUMAR <santosk.kumar.101@gmail.com>
2021-12-03 20:48:27 +0530

    Update 01_SOAL_Core_Phase_Solution_1.0.js

--- 01_SOAL_Core_Phase_Solution_1.0.js
+++ 01_SOAL_Core_Phase_Solution_1.0.js
@@ -25,6 +25,17 @@ let samp = [1,2,3,4,5,6,7,8];
 sort(samp);
 
 
+// Question 2:- 
+// Construct an object which contains a school's detail like cohort, mame, learners and address using a constructor function or a class.
+ 
+//  Example of object:-
+//  {
+//     name: "name of school",
+//     learners: ["Learner1", "Learner2","Learner3"],
+//     address:"Mumbai";
+//  } 
+//  Create a method inside constructor function or class which prints the name of the school.
+
 
   
     
