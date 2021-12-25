eba8130b6f4155d51360b1dea5e489f6eacbf668
SANTOSH KUMAR <santosk.kumar.101@gmail.com>
2021-12-25 16:27:04 +0530

    Update 01_SOAL_Core_Phase_Solution_1.0.js

--- 01_SOAL_Core_Phase_Solution_1.0.js
+++ 01_SOAL_Core_Phase_Solution_1.0.js
@@ -3,7 +3,7 @@
 // Create a function that accepts an array called 'myarray' as an argument, iterates through 'myarray', 
 // and returns a new array which contains all the even number in 'myarray'.
 // Sample input: [1,2,3,4,5,6,7,8] 
-// Sample output: [2,4,6,8] 
+// Sample output: [2,4,6,8]  
  
 
 // Sol:-
