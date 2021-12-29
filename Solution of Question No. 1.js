3cf968f6dbac38b3d5a28d3c9ff00bf48e908bdf
SANTOSH KUMAR <santosk.kumar.101@gmail.com>
2021-12-29 17:23:22 +0530

    Update and rename Solution of Question No. 1 to Solution of Question No. 1.js

---
+++ Solution of Question No. 1.js
@@ -0,0 +1,15 @@
+// Question 1 :-
+// Create a function that accepts an array called 'myarray' as an argument, iterates through 'myarray', and returns a new array which contains all the even number in 'myarray'.
+// Sample input: [1,2,3,4,5,6,7,8]
+// Sample output: [2,4,6,8]
+
+function sort(p){
+  var b = [];
+  for(let i=0; i<p.length;i+){
+    if(p[i]%2==0){
+    b.push(p[i])}}return b;}
+myarray = [1,2,3,4,5,6,7,8];
+sort(myarray);
+
+    
+
