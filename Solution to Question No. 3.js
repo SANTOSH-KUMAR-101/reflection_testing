4c9372cad37ea975727d5881027d912da83cbfe8
SANTOSH KUMAR <santosk.kumar.101@gmail.com>
2021-12-29 18:55:19 +0530

    Update Solution to Question No. 3.js

--- Solution to Question No. 3.js
+++ Solution to Question No. 3.js
@@ -1,1 +1,15 @@
 
+// QUESTION NO. 3
+// Construct an object which contains a school's detail like cohort, mame, learners and address 
+//  using a constructor function or a class.
+ 
+//  Example of object:-
+//  {
+//     name: "name of school",
+//     learners: ["Learner1", "Learner2","Learner3"],
+//     address:"Mumbai";
+//  }
+ 
+//  Create a method inside constructor function or class which prints the name of the school.
+
+
