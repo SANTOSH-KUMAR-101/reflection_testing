2c4ff09ce6653738e625f097b18edf670b54b431
SANTOSH KUMAR <santosk.kumar.101@gmail.com>
2021-12-29 18:44:18 +0530

    Update Solution to Question No. 2.js

--- Solution to Question No. 2.js
+++ Solution to Question No. 2.js
@@ -1,2 +1,21 @@
 
 // Write a test for a function that accepts an array and return all the even numbers in that array.
+
+const sort = require("./sort1");
+
+test("Sort Array No even Number", () => {
+  expect(sort([1, 1, 1, 1])).toEqual([]);
+});
+
+test("Sort Array all even Number", () => {
+  expect(sort([2, 2, 2, 2])).toEqual([2, 2, 2, 2]);
+});
+
+test("Sort Array having even and odd Number", () => {
+  expect(sort([1, 2, 3, 2])).toEqual([2, 2]);
+});
+
+test("Sort Array all Zeros", () => {
+  expect(sort([0, 0])).toEqual([0, 0]);
+});
+
