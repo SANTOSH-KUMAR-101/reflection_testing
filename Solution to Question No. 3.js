43fcfca62a87a5f40d73e1dc9b3d73a80d4f716f
SANTOSH KUMAR <santosk.kumar.101@gmail.com>
2021-12-29 18:57:41 +0530

    Update Solution to Question No. 3.js

--- Solution to Question No. 3.js
+++ Solution to Question No. 3.js
@@ -13,3 +13,17 @@
 //  Create a method inside constructor function or class which prints the name of the school.
 
 
+class school {
+    constructor(p1,p2,p3,p4){
+        this.cohort = p1;
+        this.name = p2;
+        this.learners = p3;
+        this.address = p4;
+    }
+    
+    print(){console.log(this.name);}
+
+}
+
+var b = new school("pavo","name of school",["Learner1", "Learner2","Learner3"],"Mumbai");
+console.log(b);
